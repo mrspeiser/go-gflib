aaf49e03debdc1df0c5f6113b0eaa7c8522db9ab
matthew <mattrspeiser@gmail.com>
2022-03-09 16:34:59 -0700

    add more prints to tests, added string map,any,all,filter,index to lib

--- lib.go
+++ lib.go
@@ -32,20 +32,86 @@ func SetSeed(){
   rand.Seed(time.Now().UTC().UnixNano())
 }
 
-func UnsignedInt(length int) int {
-  l := length
-  if l < 1 {
-     l = 1
+func UnsignedInt(max int) int {
+  m := max
+  if m < 1 {
+     m = 1
   }
-  Int := rand.Intn(l)
+  Int := rand.Intn(m)
+  return Int
+}
+
+func PositiveUnsignedInt(max int) int {
+  Int := UnsignedInt(max)
+  if Int == 0 {
+    Int = Int+1
+  }
   return Int
 }
 
 func RandomAlnum(length int) string {
   s := make([]byte,length)
   for i := range s {
-    s[i] = alnums[rand.Intn(len(alnums))]
+    s[i] = alnums[UnsignedInt(len(alnums))]
   }
   return string(s)
 }
 
+func ArrayofStringMaps(length int) ([]map[string]string) {
+  arr := make([]map[string]string,length)
+  return arr
+}
+
+func ArrayofIntMaps(length int) ([]map[string]int) {
+  arr := make([]map[string]int,length)
+  return arr
+}
+
+func IndexOf(vs []string, s string) int {
+  for i, v := range vs {
+    if v == s {
+      return i
+    }
+  }
+  return -1
+}
+
+func Include(vs []string, s string) bool {
+  return IndexOf(vs, s) >= 0
+}
+
+func Any(vs []string, f func(string) bool) bool {
+  for _,v := range vs {
+    if f(v) {
+      return true
+    }
+  }
+  return false
+}
+
+func All(vs []string, f func(string) bool) bool {
+  for _,v := range vs {
+    if !f(v) {
+      return false
+    }
+  }
+  return true
+}
+
+func Filter(vs []string, f func(string) bool) []string {
+  nvs := make([]string,0)
+  for _,v := range nvs {
+    if f(v) {
+      nvs = append(nvs,v)
+    }
+  }
+  return nvs
+}
+
+func Map(vs []string, f func(string) string) []string {
+  nvs := make([]string, len(vs))
+  for i,v := range nvs {
+    nvs[i] = f(v)
+  }
+  return nvs
+}
--- lib_test.go
+++ lib_test.go
@@ -14,6 +14,41 @@ func TestUnsignedInt(t *testing.T){
   gfl.SetSeed()
   a := gfl.UnsignedInt(100)
   b := gfl.UnsignedInt(100)
-  fmt.Printf("\nrandom integer: %d\n",a)
-  fmt.Printf("random integer: %d\n",b)
+  fmt.Printf("\nrandom integer: %d",a)
+  fmt.Printf("\nrandom integer: %d\n",b)
+}
+
+func TestRandomAlnum(t *testing.T){
+  gfl.SetSeed()
+  uia := gfl.UnsignedInt(16)
+  uib := gfl.UnsignedInt(16)
+  uic := gfl.UnsignedInt(16)
+
+  fmt.Printf("\nrandom integer: %d",uia)
+  fmt.Printf("\nrandom integer: %d",uib)
+  fmt.Printf("\nrandom integer: %d\n",uic)
+
+  a := gfl.RandomAlnum(uia)
+  b := gfl.RandomAlnum(uib)
+  c := gfl.RandomAlnum(uic)
+
+  fmt.Printf("\nrandom alnum: %s",a)
+  fmt.Printf("\nrandom alnum: %s",b)
+  fmt.Printf("\nrandom alnum: %s\n",c)
+
+  x := gfl.RandomAlnum(24)
+  y := gfl.RandomAlnum(24)
+  z := gfl.RandomAlnum(24)
+
+  fmt.Printf("\nrandom alnum 24 chars: %s",x)
+  fmt.Printf("\nrandom alnum 24 chars: %s",y)
+  fmt.Printf("\nrandom alnum 24 chars: %s\n",z)
+}
+
+func TestArrayMaps(t *testing.T){
+  a := gfl.ArrayofStringMaps(10)
+  b := gfl.ArrayofIntMaps(10)
+  fmt.Printf("\n")
+  fmt.Println(a)
+  fmt.Println(b)
 }
